e68d3c848f5b587d8fe375279cc0c5d68d233eff
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-22 16:43:59 +0000

    many to many

---
+++ pirateServiceStart/src/test/java/com/codeclan/example/pirateservice_d1_starter/PirateserviceD1StarterApplicationTests.java
@@ -0,0 +1,37 @@
+package com.codeclan.example.pirateservice_d1_starter;
+
+import com.codeclan.example.pirateservice_d1_starter.models.Pirate;
+import com.codeclan.example.pirateservice_d1_starter.models.Ship;
+import com.codeclan.example.pirateservice_d1_starter.repositories.PirateRepository;
+import com.codeclan.example.pirateservice_d1_starter.repositories.ShipRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceD1StarterApplicationTests {
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+	@Autowired
+	ShipRepository shipRepository;
+
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void createPirateAndShip(){
+		Ship ship = new Ship("The Flying Dutchman");
+		shipRepository.save(ship);
+
+		Pirate pirate1 = new Pirate("Jack", "Sparrow", 32, ship);
+		pirateRepository.save(pirate1);
+	}
+
+}
